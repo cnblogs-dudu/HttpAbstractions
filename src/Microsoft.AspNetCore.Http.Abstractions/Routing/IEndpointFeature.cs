2a0dba20f80595852545f6d3d9df172ef7c346b1
James Newton-King <james@newtonking.com>
2018-08-09 15:39:33 +1200

    Copy core endpoint routing types to HttpAbstractions

---
+++ src/Microsoft.AspNetCore.Http.Abstractions/Routing/IEndpointFeature.cs
@@ -0,0 +1,26 @@
+﻿// Copyright (c) .NET Foundation. All rights reserved.
+// Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
+
+using Microsoft.AspNetCore.Http;
+
+namespace Microsoft.AspNetCore.Routing.Features
+{
+    /// <summary>
+    /// A feature interface for endpoint routing. Use <see cref="HttpContext.Features"/>
+    /// to access an instance associated with the current request.
+    /// </summary>
+    public interface IEndpointFeature
+    {
+        /// <summary>
+        /// Gets or sets the selected <see cref="Routing.Endpoint"/> for the current
+        /// request.
+        /// </summary>
+        Endpoint Endpoint { get; set; }
+
+        /// <summary>
+        /// Gets or sets the <see cref="RouteValueDictionary"/> associated with the currrent
+        /// request.
+        /// </summary>
+        RouteValueDictionary Values { get; set; }
+    }
+}
