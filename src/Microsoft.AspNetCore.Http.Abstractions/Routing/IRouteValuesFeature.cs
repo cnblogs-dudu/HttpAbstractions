37ffd4148066e305f76a9dbd8afbe67abf9bcdcb
James Newton-King <james@newtonking.com>
2018-08-24 13:52:47 +1200

    Docs

--- src/Microsoft.AspNetCore.Http.Abstractions/Routing/IRouteValuesFeature.cs
+++ src/Microsoft.AspNetCore.Http.Abstractions/Routing/IRouteValuesFeature.cs
@@ -5,6 +5,10 @@ using Microsoft.AspNetCore.Routing;
 
 namespace Microsoft.AspNetCore.Http.Features
 {
+    /// <summary>
+    /// A feature interface for routing values. Use <see cref="HttpContext.Features"/>
+    /// to access the values associated with the current request.
+    /// </summary>
     public interface IRouteValuesFeature
     {
         /// <summary>
